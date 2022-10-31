ab49efe5440607e1f83d6166d70b269530881069
egi verdiansyah <114469743+Egi-999@users.noreply.github.com>
2022-10-31 13:02:19 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <tittle> <h1> Hahasiswa </h1> </tittle>
+  </head>
+  <body>
+
+    <?php
+      echo "<h1> 10% kuliah </h1>";
+      echo "<h2> sisanya hahahihi </h2> <br>";
+      echo "Member = ";
+    ?>
+
+    Hahasiswa
+
+  </body>
+</html>
---
+++ tugas3.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    
+    <?php
+
+      $nama = array('egi', 'fandi', 'naoval');
+      $usia = [20, 15, 10];
+
+      echo ("usia $nama[0] adalah $usia[0] tahun");
+    ?> 
+
+  </body>
+</html>
